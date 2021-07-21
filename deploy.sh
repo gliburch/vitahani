1f72eb8a3ecdc9d204605d435d92c49f5a6688a5
gliburch <studio.gliburch@gmail.com>
2021-07-21 18:51:50 +0900

    Fix build command

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-JEKYLL_ENV=production jekyll build --config _config_general.yml &&
+JEKYLL_ENV=production bundle exec jekyll build --config _config_general.yml &&
 firebase deploy --only hosting:general &&
-JEKYLL_ENV=production jekyll build --config _config_cancer.yml &&
+JEKYLL_ENV=production bundle exec jekyll build --config _config_cancer.yml &&
 firebase deploy --only hosting:cancer
